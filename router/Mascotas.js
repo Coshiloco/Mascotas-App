cb142bd8330ca0634bcef27baa532480a43b83ad
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-06-10 16:03:34 +0200

    CambioSolucion

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 const Mascota = require('../models/mascota')
 
 router.get('/', async (req, res) => {
     
     try {
-        
+        const uri = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@cluster0.gt8orbz.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+        mongoose.connect(uri,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+        )
+            .then(() => console.log('Base de datos conectada'))
+            .catch(e => console.log(e))
         const arrayMascotasDB = await Mascota.find()
         console.log(arrayMascotasDB)
 
@@ -18,7 +25,6 @@ router.get('/', async (req, res) => {
         console.log(error)
     }
 
-    
 })
 
 module.exports = router;
