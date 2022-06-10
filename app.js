7c9ef5439059370ef42970190df9a24194f99bc9
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-06-10 14:42:11 +0200

    urls

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ mongoose.connect(uri,
     .catch(e => console.log(e))
 
 
-app.use(express.static(__dirname + "/public"));
-
-
 // motor de plantillas
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+
+
+app.use(express.static(__dirname + "/public"));
 
 
 // Rutas Web
