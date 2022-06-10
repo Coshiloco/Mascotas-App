da79f6be95884308492b6a6e7b55004765f4c00c
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-06-10 15:38:06 +0200

    cambiosOtraVez

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@primeradbmongo.ua2xmjf.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@primeradbmongo.ua2xmjf.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
 
 mongoose.connect(uri,
