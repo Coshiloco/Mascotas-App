33f0f18e909635ba100a2f9184b6d85c9e69ca04
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-06-11 11:45:12 +0200

    pr

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -40,26 +40,20 @@ router.get('/:id', async(req, res) => {
     //En  mongo DB guion bajo viene el id la propiedad del Objeto almacenado en nuestra BBDD
     const id = req.params.id
 
-    const cuerpoMascota = req.body
-
-    const NombreMascota = cuerpoMascota.nombre
-
-    const DescripcionMascota = cuerpoMascota.descripcion
-
     try {
 
         // Para que encuentre el id dentro de findOne {_id : id pasada por nosotros}
         const mascotaDB = await Mascota.findOne({_id : id})
 
+        const NombreMascota = mascotaDB.nombre
+
         console.log(mascotaDB)
+        console.log(NombreMascota)
 
         res.render('detalle', {
             mascota : mascotaDB,
             //Para que el usuario no ponga una id inexistente ponemos el error a falso
             error : false,
-            id : id,
-            NombreMascota : NombreMascota,
-            DescripcionMascota : DescripcionMascota
         })
 
     } catch (error) {
@@ -68,9 +62,7 @@ router.get('/:id', async(req, res) => {
         res.render('detalle', {
             error : true,
             mensaje : "No se encuentra el id del objeto que usted selecciono",
-            id : id,
-            NombreMascota : NombreMascota,
-            DescripcionMascota : DescripcionMascota
+            id : id
         })
     }
 })
