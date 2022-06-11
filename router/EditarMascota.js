3fda3c4c79532c10aed050d2c3ce04a8cf55b724
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-06-11 18:52:57 +0200

    editar

--- router/EditarMascota.js
+++ router/EditarMascota.js
@@ -7,13 +7,11 @@ const Mascota = require('../models/mascota')
 router.get('/:id/editar', async(req, res) => {
     const id = req.params.id
     const mascotaDB = await Mascota.find({_id : id})
-    console.log(mascotaDB)
     res.render('editar', {
         mascota : mascotaDB,
         id : id
     })
     console.log(res.mascota)
 })
-
 
 module.exports = router;
